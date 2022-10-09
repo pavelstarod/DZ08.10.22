cb7e10ab3aa1dd84df07dfdc40a609647c80659f
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-09 11:56:42 +0300

    add dz01

---
+++ dz01/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите N2: ");
+int N = 1;
+int N2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Вывод кубов числа из отрезка: ");
+
+while (N <= N2)
+{ 
+    Console.WriteLine(N*N*N);
+    N++;
+}
+    
