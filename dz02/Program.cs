7a214de20bb0245bef3d65f72515131451c1b81f
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-14 20:37:48 +0300

    ad dz02

---
+++ dz02/Program.cs
@@ -0,0 +1,16 @@
+﻿//Подсчитать сумму цифр в числе
+
+
+
+Console.WriteLine("Введите число: ");
+int n = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (n / 10 != 0 || n % 10 != 0)
+{
+    sum = sum + (n % 10);
+    n = n / 10;
+}
+
+Console.WriteLine("Вывод суммы цифр числа: " + sum);
+
