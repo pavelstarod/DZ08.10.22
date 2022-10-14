cb89dd6615eb68c38f294230d18176b832f75c52
Pavel Starodubov <pablostar68@yahoo.com>
2022-10-14 21:00:29 +0300

    add dz03

---
+++ dz03/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите N2: ");
+int N2 = int.Parse(Console.ReadLine() ?? "0");
+int N1 = 1;
+int result = 1;
+
+while (N1 <= N2)
+{
+    result = result * N1;
+    N1++;
+}
+Console.WriteLine("Вывод произведения цифр числа: " + result);
